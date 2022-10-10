f9baf4cac1de629e52e285ca15fc809979db7f13
Mark McGoey <mmcgoey@cs.ualberta.ca>
2022-10-10 12:15:14 -0600

    add httpclient.py

--- httpclient.py
+++ httpclient.py
@@ -36,12 +36,14 @@ from sys import getsizeof
 CITATIONS: 
 My get_headers(self,data) and get_body(self,data) function is based off the code snippet from the stack over flow website linked below
 https://stackoverflow.com/questions/8474745/how-do-i-get-the-body-of-a-http-response-from-a-string-containing-the-entire-res
-The code I used from this web site was an answer provided by bogdan on Dec 12, 2011
+These functions are based off an answer provided by bogdan on Dec 12, 2011
 
 My get_remote_ip(self,host) function is taken from the provided code in lab 2 client.py 
 
 Other sites and resources I referred to while completing the assignment:
+
 The 404 lecture slides on HTTP
+
 https://stackoverflow.com/questions/45695168/send-raw-post-request-using-socket,
 https://www.internalpointers.com/post/making-http-requests-sockets-python,
 https://www.geeksforgeeks.org/get-post-requests-using-python/,
@@ -103,7 +105,7 @@ class HTTPClient(object):
         # CITATION BELOW
         # https://stackoverflow.com/questions/8474745/how-do-i-get-the-body-of-a-http-response-from-a-string-containing-the-entire-res
 
-        # I used the ideas suggest in this stack over flow post to seperate the body from the headers
+        # I used the ideas suggested in this stack over flow post to seperate the body from the headers
 
 
         find_index = data.find('\r\n\r\n')
@@ -214,7 +216,7 @@ class HTTPClient(object):
         # If no args are provided then there's no need to encode anything
         if args != None:
             args_encode = urlencode(args)
-            
+  
             response = 'POST %s HTTP/1.1\r\nHost: %s:%d\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept: application/x-www-form-urlencoded\r\nContent-Length: %d\r\nConnection: close\r\n\r\n'%(url,host,port,len(args_encode)) + args_encode 
         else:
             response = 'POST %s HTTP/1.1\r\nHost: %s:%d\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept: application/x-www-form-urlencoded\r\nContent-Length: 0\r\nConnection: close\r\n\r\n'%(url,host,port) 
